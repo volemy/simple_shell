74992be1edcef1110a8caf7f9b0ab424990f7be2
volemy <minayobenevolence0@gmail.com>
2023-09-20 23:30:24 +0000

    exit shell

--- exit.c
+++ exit.c
@@ -1,11 +1,30 @@
 #include "main.h"
 
 /**
- * exit_shell - function that exits the shell
- * Return: 0 (success)
+ * exit_shell -  function that exits the shell
+ * @arg: arguments after the exit command
+ * Return: (0)
+ *
  */
-
-void exit_shell(void)
+int exit_shell(const char *arg)
 {
-	exit(0);
+	if (arg)
+	{
+		int exit_code = atoi(arg);
+
+		if (exit_code == 0 && arg[0] != 0)
+		{
+
+			write(STDERR_FILENO, "Illegal number: ", 16);
+			write(STDERR_FILENO, arg, strlen(arg));
+			write(STDERR_FILENO, "\n", 1);
+			return (1);
+		}
+		exit(exit_code);
+	} else
+	{
+		exit(0);
+	}
+
+	return (0);
 }
